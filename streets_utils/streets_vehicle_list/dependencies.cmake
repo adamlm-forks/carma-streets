ae2e34eb616a15c473503853e7639059cbc78bde
Adam Morrissett <me@adamlm.com>
2022-10-16 19:18:16 -0400

    streets_vehicle_list: Make GTest conditional
    GTest was previously required in all instances, but it is only actually
    required when unit tests are being built. If this project is consumed by
    another project, then by default tests will not be built. In this
    scenario, there is an unnecessary requirement on GTest.
    
    streets_utils/streets_vehicle_list/dependencies.cmake
    * make GTest find_package conditioned on building tests

--- streets_utils/streets_vehicle_list/dependencies.cmake
+++ streets_utils/streets_vehicle_list/dependencies.cmake
@@ -1,3 +1,6 @@
 find_package(spdlog REQUIRED)
 find_package(RapidJSON REQUIRED)
-find_package(GTest REQUIRED)
+
+if(STREETSVEHICLELIST_ENABLE_TESTING OR PROJECT_IS_TOP_LEVEL)
+  find_package(GTest REQUIRED)
+endif()
